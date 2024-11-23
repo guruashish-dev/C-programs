348654f7d878a449a2981e52fdd77f36a9c54e30
Guru Ashish <guru06ashish@gmail.com>
2024-11-23 10:49:21 +0530

    Update celcius_to_fahrenheit.c

--- celcius_to_fahrenheit.c
+++ celcius_to_fahrenheit.c
@@ -1,15 +1,13 @@
-// // to convert celcius to fahrenheit
-// #include<stdio.h>
-// int main()
-// {
-//     int c;
-//     float f;
-//     printf("enter value in celcius: ");
-//     scanf ("%d", &c);
+#include<stdio.h>
+int main()
+{
+    int c;
+    float f;
+    printf("enter value in celcius: ");
+    scanf("%d", &c);
 
-//     f = (c * 9.0/5.0 + 32);
+    f = (c * 9.0 / 5.0 + 32);
     
-//     printf("fahrenheit: %f", f);
-//     return 0;
-
-// }
+    printf("fahrenheit: %f", f);
+    return 0;
+}
