b9cedc774b24fa8c71d7780d83f4f9f7d799adac
Guru Ashish <guru06ashish@gmail.com>
2024-11-23 10:46:25 +0530

    Add files via upload

---
+++ loops.c
@@ -0,0 +1,24 @@
+// FOR LOOPS
+
+// #include<stdio.h>
+// int main(){
+
+// for (int i = 1; i <= 5; i++)
+// {
+//     printf("Number: %d\n", i);
+// }
+// }
+
+// ___________________________________________
+
+//  #include<stdio.h>
+//  int main(){
+
+//     int count = 1;
+//     while (count<=5)
+//     {
+//         printf("count: %d\n/", count);
+//         count++;
+//     }
+
+//  }
