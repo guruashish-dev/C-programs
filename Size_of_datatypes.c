ea0dc8c20e8bf79982650de2bed9e75a4368a0f2
Guru Ashish <guru06ashish@gmail.com>
2024-11-23 10:51:58 +0530

    Add files via upload

---
+++ Size_of_datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () {
+  printf("char size = %d bytes\n", sizeof(char));
+  printf("short size = %d bytes\n", sizeof(short));
+  printf("int size = %d bytes\n", sizeof(int));
+  printf("long size = %d bytes\n", sizeof(long));
+  printf("float size = %d bytes\n", sizeof(float));
+  printf("float size = %d bytes\n", sizeof(double));
+
+  return 0;
+
+}
